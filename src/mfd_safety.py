b3f2d4bd7743e05a70701a98a261cd2a150e2239
Manuel Caceres <el.ariel.cl@gmail.com>
2022-09-01 11:41:21 +0300

    Refactor of Group testing

--- src/mfd_safety.py
+++ src/mfd_safety.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import os
+import sys
 import argparse
 import networkx as nx
 import gurobipy as gp
@@ -9,11 +10,13 @@ from gurobipy import GRB
 
 
 def get_edge(raw_edge):
+
     parts = raw_edge.split()
     return int(parts[0]), int(parts[1]), float(parts[2])
 
 
 def get_graph(raw_graph):
+
     graph = {
         'n': 0,
         'edges': list()
@@ -29,9 +32,15 @@ def get_graph(raw_graph):
         return graph
 
 
-def read_input(graph_file):
+def read_input_graphs(graph_file):
+
     graphs_raw = open(graph_file, 'r').read().split('#')[1:]
     return [get_graph(raw_g) for raw_g in graphs_raw]
+
+
+def read_input(graph_file):
+
+    return read_input_graphs(graph_file)
 
 
 def mfd_algorithm(data):
@@ -46,6 +55,7 @@ def mfd_algorithm(data):
 
 
 def solve_instances(graphs):
+
     output = list()
     for graph in graphs:
 
@@ -94,7 +104,7 @@ def solve_instances(graphs):
     return output
 
 
-def build_ilp_model(data, size):
+def build_base_ilp_model(data, size):
 
     vertices = data['vertices']
     edges = data['edges']
@@ -144,22 +154,48 @@ def build_ilp_model(data, size):
     return model, x, w, z
 
 
+def build_ilp_model_avoiding_path(data, size, path):
+
+    model, x, _, _ = build_base_ilp_model(data, size)
+
+    # safety test constraint
+    for k in range(size):
+        model.addConstr(sum(x[u, v, k] for (u, v) in path) <= len(path) - 1)
+
+    return model
+
+
 def get_solution(model, data, size):
 
-    edges = data['edges']
-    T = [(u, v, k) for (u, v) in edges for k in range(size)]
+    if model.status == GRB.OPTIMAL:
+        edges = data['edges']
+        T = [(u, v, k) for (u, v) in edges for k in range(size)]
 
-    w_sol = [0] * len(range(size))
-    paths = [list() for _ in range(size)]
-    for k in range(size):
-        w_sol[k] = model.getVarByName(f'w[{k}]').x
-    for (u, v, k) in T:
-        if model.getVarByName(f'x[{u},{v},{k}]').x == 1:
-            paths[k].append((u, v))
-    for k in range(len(paths)):
-        paths[k] = sorted(paths[k])
+        w_sol = [0] * len(range(size))
+        paths = [list() for _ in range(size)]
+        for k in range(size):
+            w_sol[k] = model.getVarByName(f'w[{k}]').x
+        for (u, v, k) in T:
+            if model.getVarByName(f'x[{u},{v},{k}]').x == 1:
+                paths[k].append((u, v))
+        for k in range(len(paths)):
+            paths[k] = sorted(paths[k])
 
-    return w_sol, paths
+        data['weights'], data['solution'] = w_sol, paths
+
+    return data
+
+
+def update_status(data, model):
+
+    if model.status == GRB.OPTIMAL:
+        data['message'] = 'solved'
+        data['runtime'] = model.Runtime
+
+    if model.status == GRB.INFEASIBLE:
+        data['message'] = 'unsolved'
+
+    return data
 
 
 def fd_fixed_size_forbidding_path(data, size, path):
@@ -167,29 +203,15 @@ def fd_fixed_size_forbidding_path(data, size, path):
     # calculate a flow decomposition into size paths avoiding path
     try:
         # Create a new model
-        model, x, _, _ = build_ilp_model(data, size)
-
-        # safety test constraint
-        for k in range(size):
-            model.addConstr(sum(x[u, v, k] for (u, v) in path) <= len(path) - 1)
-
-        # objective function
+        model = build_ilp_model_avoiding_path(data, size, path)
         model.optimize()
-
-        if model.status == GRB.OPTIMAL:
-            data['message'] = 'solved'
-            data['runtime'] = model.Runtime
-            data['weights'], data['solution'] = get_solution(model, data, size)
-
-        if model.status == GRB.INFEASIBLE:
-            data['message'] = 'unsolved'
-
+        data = update_status(data, model)
 
     except gp.GurobiError as e:
-        print('Error code ' + str(e.errno) + ': ' + str(e))
+        print(f'Error code {e.errno}: {e}', file=sys.stderr)
 
     except AttributeError:
-        print('Encountered an attribute error')
+        print('Encountered an attribute error', file=sys.stderr)
 
     return data
 
@@ -203,29 +225,24 @@ def fd_fixed_size(data, size):
     # calculate a flow decomposition into size paths
     try:
         # Create a new model
-        model, _, _, _ = build_ilp_model(data, size)
+        model, _, _, _ = build_base_ilp_model(data, size)
 
         # objective function
         model.optimize()
-
-        if model.status == GRB.OPTIMAL:
-            data['message'] = 'solved'
-            data['runtime'] = model.Runtime
-            data['weights'], data['solution'] = get_solution(model, data, size)
-
-        if model.status == GRB.INFEASIBLE:
-            data['message'] = 'unsolved'
+        data = update_status(data, model)
+        data = get_solution(model, data, size)
 
     except gp.GurobiError as e:
-        print('Error code ' + str(e.errno) + ': ' + str(e))
+        print(f'Error code {e.errno}: {e}', file=sys.stderr)
 
     except AttributeError:
-        print('Encountered an attribute error')
+        print('Encountered an attribute error', file=sys.stderr)
 
     return data
 
 
 def solve_instances_safety(graphs, output_file):
+
     mfds = solve_instances(graphs)
 
     output = open(output_file, 'w+')
@@ -233,8 +250,8 @@ def solve_instances_safety(graphs, output_file):
 
     for g, mfd in enumerate(mfds):
 
-        print(f"# graph {g}", file=output)
-        print(f"# graph {g}", file=output_counters)
+        output.write(f"# graph {g}\n")
+        output_counters.write(f"# graph {g}\n")
         paths = mfd['solution']
 
         ilp_count = 0  # Number of ILP calls per graph
@@ -272,35 +289,39 @@ def solve_instances_safety(graphs, output_file):
 
             # print path
             for max_safe in maximal_safe_paths:
-                print(-1, *sorted(set([item for t in max_safe for item in t])), sep=" ", file=output)
+                output.write('-1 ')
+                output.write(' '.join(map(str, sorted(set([item for t in max_safe for item in t])))))
+                output.write('\n')
             
-            print(-1, ilp_count, file=output_counters)
+            output_counters.write(f'{ilp_count}\n')
 
     output.close()
     output_counters.close()
 
 
-parser = argparse.ArgumentParser(
-    description="""
-    Decompose a network flow into a minimum number of weighted paths. 
-    This script uses the Gurobi ILP solver.
-    """,
-    formatter_class=argparse.RawTextHelpFormatter
-)
-parser.add_argument('-wt', '--weighttype', type=str, default='int+',
-                    help='Type of path weights (default int+):\n   int+ (positive non-zero ints), \n   float+ (positive non-zero floats).')
-parser.add_argument('-t', '--threads', type=int, default=0,
-                    help='Number of threads to use for the Gurobi solver; use 0 for all threads (default 0).')
+if __name__ == '__main__':
 
-requiredNamed = parser.add_argument_group('required arguments')
-requiredNamed.add_argument('-i', '--input', type=str, help='Input filename', required=True)
-requiredNamed.add_argument('-o', '--output', type=str, help='Output filename', required=True)
+    parser = argparse.ArgumentParser(
+        description="""
+        Computes maximal safe paths for Minimum Flow Decomposition. 
+        This script uses the Gurobi ILP solver.
+        """,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('-wt', '--weighttype', type=str, default='int+',
+                        help='Type of path weights (default int+):\n   int+ (positive non-zero ints), \n   float+ (positive non-zero floats).')
+    parser.add_argument('-t', '--threads', type=int, default=0,
+                        help='Number of threads to use for the Gurobi solver; use 0 for all threads (default 0).')
 
-args = parser.parse_args()
+    requiredNamed = parser.add_argument_group('required arguments')
+    requiredNamed.add_argument('-i', '--input', type=str, help='Input filename', required=True)
+    requiredNamed.add_argument('-o', '--output', type=str, help='Output filename', required=True)
 
-threads = args.threads
-if threads == 0:
-    threads = os.cpu_count()
-print(f"INFO: Using {threads} threads for the Gurobi solver")
+    args = parser.parse_args()
 
-solve_instances_safety(read_input(args.input), args.output)
+    threads = args.threads
+    if threads == 0:
+        threads = os.cpu_count()
+    print(f"INFO: Using {threads} threads for the Gurobi solver")
+
+    solve_instances_safety(read_input(args.input), args.output)
