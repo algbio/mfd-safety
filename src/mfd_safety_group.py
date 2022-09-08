e615f875bc8ec096018af611e5d6dcd62d8c17ab
Lucy Williams <lgw2@uw.edu>
2022-09-08 17:22:20 -0600

    group testing for extending single edges from mfd working (input/output needs cleanup)

--- src/mfd_safety_group.py
+++ src/mfd_safety_group.py
@@ -98,43 +98,136 @@ def solve_group_testing_example(graphs, group_tests, output_file):
     output.close()
 
 
+def group_test_paths(paths, mfd):
+    """
+    Input: a set of paths in a graph, and the graph data (mfd variable)
+    Output: the subset of the paths that are safe for MFD.
+    This implements algorithm 3 from the MFD safety with ILP paper.
+    """
+
+    model = build_ilp_model_avoiding_multiple_paths(mfd,
+                                                    len(mfd['solution']),
+                                                    paths)
+    model.optimize()
+
+    N = get_unsafe_paths(model, paths)
+    if not N:
+        return paths
+    else:
+        # call on paths \ N
+        return group_test_paths(list(set(paths).difference(set(N))), mfd)
+
+
+def left_extensions(core):
+    to_return = []
+    for i, path in enumerate(core["paths"]):
+        for (left, right) in core[i]:
+            if left != 0:
+                to_return.append(tuple(path[left - 1:right + 1]))  # right not inclusive
+    return to_return
+
+
+def right_extensions(core):
+    to_return = []
+    for i, path in enumerate(core["paths"]):
+        for (left, right) in core[i]:
+            if right != len(path) - 1:
+                to_return.append(tuple(path[left:right + 2]))  # right not inclusive
+    return to_return
+
+
+def update_core(safe_paths, core):
+    """
+    Move the left and right indices for all core subpaths based on what was
+    safe.
+    """
+    for i, path in enumerate(core["paths"]):
+        new_core = []
+        for (left, right) in core[i]:
+            if left != 0:
+                # check left extension
+                subpath = tuple(path[left - 1:right + 1])
+                if subpath in safe_paths:
+                    new_core.append((left-1, right))
+            if right != len(path) - 1:
+                # check right extension
+                subpath = tuple(path[left: right + 2])
+                if subpath in safe_paths:
+                    new_core.append((left, right + 1))
+        core[i] = sorted(list(set(new_core)))
+
+
+def get_maximal_safe(core, mfd, max_safe_paths):
+    """
+    Input: a core set of paths and graph data (mfd). If the core set is a safe
+    core for the graph, then this returns all maximal safe paths.
+    Output: maximal safe paths generated from the core set.
+    This implements algorithm 4 from the MFD safety with ILP paper.
+    """
+    paths = list(set(left_extensions(core) + right_extensions(core)))
+    safe_paths = group_test_paths(paths, mfd)
+    # if the left extension is not safe and the right extension is not safe,
+    # then the original path was safe
+    for i, path in enumerate(core["paths"]):
+        # print(f"checking for path {path}")
+        for (left, right) in core[i]:
+            # print(f" checking ({left}, {right})")
+            # print(f"  left extension is {path[left - 1: right + 1]}")
+            # print(f"  right extension is {path[left: right + 2]}")
+            left_unsafe = tuple(path[left - 1: right + 1]) not in safe_paths
+            right_unsafe = tuple(path[left: right + 2]) not in safe_paths
+            if left_unsafe and right_unsafe:
+                max_safe_paths.append(path[left:right + 1])
+    update_core(safe_paths, core)
+    # print("new core is:")
+    # for i, p in enumerate(core["paths"]):
+    #     print(core[i])
+
+    # count how many core sets are left
+    total_core = 0
+    for i, path in enumerate(core["paths"]):
+        total_core += len(core[i])
+
+    if total_core > 0:
+        get_maximal_safe(core, mfd, max_safe_paths)
+
+
 def solve_group_testing(graphs, output_file):
     # goal for now: do a group test for each path with all length 2 subpaths.
-    print("In solve group testing")
 
     mfds = solve_instances(graphs)
 
-    # okay, we do want unsafe to be written out.
-    output = open(output_file, 'w+')
-
     for g, mfd in enumerate(mfds):
-
-        # start output file
-        output.write(f"# graph {g}\n")
 
         # for each path in mfd, test all len 2 subpaths
         paths = mfd['solution']
+        # core is a dictionary associating a bunch of subpaths (identified by
+        # their l and r) to each path. to start, core is just length 1
+        # subpaths (single edges).
+        # e.g., if the 0th path is [(0,1),(1,11),(11,3),(3,12)], then
+        # core[0] = [(0,0), (1,1), (2,2), (3,3)]
+        core = dict()
+        core["paths"] = paths
+        for i, path in enumerate(paths):
+            # core[i] = all length 1 path indices for ith path
+            core[i] = list(zip(list(range(len(path))),
+                               list(range(len(path)))))
+
+        max_safe_paths = []
+        get_maximal_safe(core, mfd, max_safe_paths)
+        for x in max_safe_paths:
+            print(x)
+        """
         for path in paths:
             len2paths = list(zip(path, path[1:]))
             try:
-                model = build_ilp_model_avoiding_multiple_paths(mfd,
-                                                                len(mfd['solution']),
-                                                                len2paths)
-                model.optimize()
-
-                for unsafe_path in get_unsafe_paths(model, len2paths):
-                    output.write(' '.join(map(str, sorted(set([item for t in unsafe_path for item in t])))))
-                    output.write('\n')
-
+                safe_paths = group_test_paths(len2paths, mfd)
             except gp.GurobiError as e:
                 print(f'Error code {e.errno}: {e}', file=sys.stderr)
 
             except AttributeError:
                 print('Encountered an attribute error', file=sys.stderr)
-
-
-    output.close()
-
+                """
 
 
 if __name__ == '__main__':
